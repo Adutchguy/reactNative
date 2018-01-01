73ebf1f21b14f5af6358fec9053ff3668fa6c561
Michael Miller <mdm_8@hotmail.com>
2017-12-31 18:20:54 -0800

    some stuff

--- App.js
+++ App.js
@@ -1,23 +1,15 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <View>
+        <Image
+          style={{width: 375, height: 150}}
+          source={require('./resources/imgs/cover.jpg')}
+        />
       </View>
     );
   }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+};
